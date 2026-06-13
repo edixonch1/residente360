515c6c0315cdbf1a4b75bba6cf8798a959403e58
edixonch1 <edixonch1@gmail.com>
2026-06-13 16:02:14 -0500

    database.py

--- database.py
+++ database.py
@@ -1,18 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# 🔴 PEGA AQUÍ TU URL DE SUPABASE
-DATABASE_URL = "postgresql://postgres:Chingate123%23@db.xxx.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres:Chingate123%2F@db.sidxhgsiyjaduolpeahs.supabase.co:5432/postgres"
 
-# ⚙️ Motor de base de datos
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=300
 )
 
-# 🧠 Sesión
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 🧱 Base para modelos
 Base = declarative_base()
